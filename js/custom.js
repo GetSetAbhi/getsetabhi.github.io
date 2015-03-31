b9132252c2f81596c14b8c51e9f349b94988951d
Abhishek Raina <araina97@gmail.com>
2015-03-31 23:20:34 +0530

    Added javascript file

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,8 @@
-
-window.onload = function() {
-	newTabLinks();
-}
-
-
 function newTabLinks() {
 	var links = document.getElementsByTagName('a');
 	links.setAttribute("target", "_blank");
+}
+
+window.onload = function() {
+	newTabLinks();
 }
